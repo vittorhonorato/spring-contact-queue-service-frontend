0c8539558412ba5144e96f13899d6d520673622c
Vitor Honorato <vittorhonoratoo@gmail.com>
2026-04-27 10:56:22 -0300

    Lógica de status por cores

--- contact-frontend/src/app/features/contact/components/contact-list/contact-list.component.ts
+++ contact-frontend/src/app/features/contact/components/contact-list/contact-list.component.ts
@@ -9,4 +9,16 @@ import { ContactDetailsResponse } from '../../models/contact-details-response';
 export class ContactListComponent {
   @Input() contacts: ContactDetailsResponse[] = [];
   @Input() displayedColumns: string[] = [];
+
+  getStatusClass(status: string): string {
+    switch (status?.toUpperCase()) {
+      case 'SENT':
+        return 'status-badge--sent';
+      case 'ERROR':
+        return 'status-badge--error';
+      case 'PENDING':
+      default:
+        return 'status-badge--pending';
+    }
+  }
 }
